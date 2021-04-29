8e214f769d37ee04e40dcdb8f6d96f19876e3e2e
Jen <31697704+jeniswitch@users.noreply.github.com>
2021-04-28 22:23:45 -0700

    Update longest-univalue-path.cs

--- longest-univalue-path/longest-univalue-path.cs
+++ longest-univalue-path/longest-univalue-path.cs
@@ -13,39 +13,26 @@
  */
 
 public class Solution {
-    public class Node{
-        public int val;
-        public int count;
-        public Node(int val, int count) {
-            this.val = val;
-            this.count = count;
-        }
-    }
     private int len;
     public int LongestUnivaluePath(TreeNode root) {
         Helper(root);
-        return Math.Max(0, len - 1);
+        return Math.Max(0, len);
     }
-    private Node Helper(TreeNode node) {
+    private int Helper(TreeNode node) {
         if(node == null) {
-            return null;
+            return 0;
         }
-        if(node.left == null && node.right == null) {
-            return new Node(node.val, 1);
+        int left = Helper(node.left);
+        int right = Helper(node.right);
+        int leftCount = 0;
+        int rightCount = 0;
+        if(node.left != null && node.val == node.left.val) {
+            leftCount = left + 1;
         }
-        Node left = Helper(node.left);
-        Node right = Helper(node.right);
-        int count = 1;
-        int max = 0;
-        if(left != null && node.val == left.val) {
-            count += left.count;
-            max = Math.Max(left.count, max);
+        if(node.right != null && node.val == node.right.val) {
+            rightCount = right + 1;
         }
-        if(right != null && node.val == right.val) {
-            count += right.count;
-            max = Math.Max(right.count, max);
-        }
-        len = Math.Max(count, len);
-        return new Node(node.val, max + 1);
+        len = Math.Max(leftCount + rightCount, len);
+        return Math.Max(leftCount, rightCount);
     }
 }
