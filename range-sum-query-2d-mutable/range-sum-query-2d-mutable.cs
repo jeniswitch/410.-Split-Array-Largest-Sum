807e4e1c2a3c9747036e96028664392e2594d727
Jen <31697704+jeniswitch@users.noreply.github.com>
2021-05-05 18:25:05 -0700

    Time: 152 ms (77.94%), Space: 35.2 MB (76.47%) - LeetHub

--- range-sum-query-2d-mutable/range-sum-query-2d-mutable.cs
+++ range-sum-query-2d-mutable/range-sum-query-2d-mutable.cs
@@ -1,71 +1,41 @@
 public class NumMatrix {
-    public class BIT {
-        public int[][] bit;
-        public BIT(int[][] matrix) {
-            int m = matrix.Length + 1;
-            int n = matrix[0].Length + 1;
-            bit = new int[m][];
-            for(int i = 0; i < m; i++) {
-                bit[i] = new int[n];
-            }
-            BuildTree(matrix);
-        }
-        private void BuildTree(int[][] matrix) {
-            for(int i = 1; i < bit.Length; i++) {
-                for(int j = 1; j < bit[0].Length; j++) {
-                    Update(i, j, matrix[i - 1][j - 1]);
-                }
-            }
-        }
-        public void Update(int i, int j, int val) {
-            //int indexi = i;
-            //int indexj = j;
-            /*while(indexi < bit.Length) {
-                while(indexj < bit[0].Length) {
-                    bit[indexi][indexj] += val;
-                    indexj += indexj & (-indexj);
-                }
-                indexi += indexi & (-indexi);
-            }*/
-            for(int indexi = i; indexi < bit.Length; indexi += indexi & (-indexi)) {
-                for(int indexj = j; indexj < bit[0].Length; indexj += indexj & (-indexj)) {
-                    bit[indexi][indexj] += val;
-                }
-            }
-        }
-        public int Get(int i, int j) {
-            //int indexi = i;
-            //int indexj = j;
-            int sum = 0;
-            /*while(indexi > 0) {
-                while(indexj > 0) {
-                    sum += bit[indexi][indexj];
-                    indexj -= indexj & (-indexj);
-                }
-                indexi -= indexi & (-indexi);
-            }*/
-            for(int indexi = i; indexi > 0; indexi -= indexi & (-indexi)) {
-                for(int indexj = j; indexj > 0; indexj -= indexj & (-indexj)) {
-                    sum += bit[indexi][indexj];
-                }
-            }
-            return sum;
-        }
-    }
-    private BIT tree;
-    private int[][] localMatrix;
+    private int[][] bit;
     public NumMatrix(int[][] matrix) {
-        tree = new BIT(matrix);
-        localMatrix = matrix;
+        int m = matrix.Length + 1;
+        int n = matrix[0].Length + 1;
+        bit = new int[m][];
+        for(int i = 0; i < m; i++) {
+            bit[i] = new int[n];
+        }
+        //build tree
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                UpdateBIT(i, j, matrix[i - 1][j - 1]);
+            }
+        }
     }
-    
+    private void UpdateBIT(int row, int col, int val) {
+        for(int i = row; i < bit.Length; i += i & -i) {
+            for(int j = col; j < bit[0].Length; j += j & -j) {
+                bit[i][j] += val;
+            }
+        }
+    }
+    private int GetBIT(int row, int col) {
+        int sum = 0;
+        for(int i = row; i > 0; i -= i & -i) {
+            for(int j = col; j > 0; j -= j & -j) {
+                sum += bit[i][j];
+            }
+        }
+        return sum;
+    }
     public void Update(int row, int col, int val) {
-        int diff = val - SumRegion(row, col, row, col);
-        tree.Update(row + 1, col + 1, diff);
+        UpdateBIT(row + 1, col + 1, val - SumRegion(row, col, row, col));
     }
     
     public int SumRegion(int row1, int col1, int row2, int col2) {
-        return tree.Get(row2 + 1, col2 + 1) - tree.Get(row1, col2 + 1) - tree.Get(row2 + 1, col1) + tree.Get(row1, col1);
+        return GetBIT(row2 + 1, col2 + 1) - GetBIT(row1, col2 + 1) - GetBIT(row2 + 1, col1) + GetBIT(row1, col1);
     }
 }
 
