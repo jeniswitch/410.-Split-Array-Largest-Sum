a1611bf6cc250e01d3a27a92f7bf3ca146cdd42f
Jen <31697704+jeniswitch@users.noreply.github.com>
2021-05-11 10:28:35 -0700

    Update shortest-unsorted-continuous-subarray.cs

--- shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.cs
+++ shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.cs
@@ -1,49 +1,21 @@
 public class Solution {
-    private int min;
-    private int max;
     public int FindUnsortedSubarray(int[] nums) {
-        min = nums.Length;
-        max = -1;
-        MergeSort(nums, 0, nums.Length - 1);
-        if(min > max) {
-            return 0;
-        }
-        return max - min + 1;
-    }
-    private void MergeSort(int[] nums, int start, int end) {
-        if(start >= end) {
-            return;
-        }
-        int mid = start + (end - start) / 2;
-        MergeSort(nums, start, mid);
-        MergeSort(nums, mid + 1, end);
-        
-        int left = start;
-        int right = mid + 1;
-        List<int> temp = new List<int>();
-        while(left <= mid && right <= end) {
-            //Console.WriteLine("left right " + left + " " + right);
-            if(nums[left] <= nums[right]) {
-                temp.Add(nums[left++]);
+        int min = nums.Length;
+        int max = 0;
+        var stack = new Stack<int>();
+        for(int i = 0; i < nums.Length; i++) {
+            while(stack.Count > 0 && nums[i] < nums[stack.Peek()]) {
+                min = Math.Min(min, stack.Pop());
             }
-            else {
-                //Console.WriteLine("before " + min + " " + max);
-                //Console.WriteLine("left right " + left + " " + right);
-                min = Math.Min(min, left);
-                max = Math.Max(max, right);
-                Console.WriteLine(min + " " + max);
-                temp.Add(nums[right++]);
+            stack.Push(i);
+        }
+        stack.Clear();
+        for(int i = nums.Length - 1; i >= 0; i--) {
+            while(stack.Count > 0 && nums[stack.Peek()] < nums[i]) {
+                max = Math.Max(max, stack.Pop());
             }
+            stack.Push(i);
         }
-        while(left <= mid) {
-            temp.Add(nums[left++]);
-        }
-        while(right <= end) {
-            temp.Add(nums[right++]);
-        }
-        int index = 0;
-        for(int i = start; i <= end; i++) {
-            nums[i] = temp[index++];
-        }
+        return max > min ? max - min + 1 : 0;
     }
 }
