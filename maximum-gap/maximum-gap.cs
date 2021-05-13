3ca0f15588ddef783caa63eeb181c8cc48512ab2
Jen <31697704+jeniswitch@users.noreply.github.com>
2021-05-13 12:14:04 -0700

    Time: 100 ms (28.21%), Space: 25.7 MB (20.51%) - LeetHub

--- maximum-gap/maximum-gap.cs
+++ maximum-gap/maximum-gap.cs
@@ -3,13 +3,39 @@ public class Solution {
         if(nums.Length < 2) {
             return 0;
         }
-        int[] clone = new int[nums.Length];
-        Array.Copy(nums, clone, nums.Length);
-        Array.Sort(clone);
-        int max = 0;
-        for(int i = 1; i < clone.Length; i++) {
-            max = Math.Max(clone[i]- clone[i - 1], max);
+        int min = Int32.MaxValue;
+        int max = Int32.MinValue;
+        for(int i = 0; i < nums.Length; i++) {
+            min = Math.Min(min, nums[i]);
+            max = Math.Max(max, nums[i]);
         }
-        return max;
+        Console.WriteLine(min + " " + max);
+        int[] bucketMin = new int[nums.Length - 1];
+        int[] bucketMax = new int[nums.Length - 1];
+        Array.Fill(bucketMin, Int32.MaxValue);
+        Array.Fill(bucketMax, Int32.MinValue);
+        int bucketSize = (int)Math.Ceiling((double)(max - min) / (nums.Length - 1));
+        
+        //update min and max in each bucket
+        foreach(int num in nums) {
+            if(num > min && num < max) {
+                int idx = (num - min) / bucketSize;
+                bucketMin[idx] = Math.Min(num, bucketMin[idx]);
+                bucketMax[idx] = Math.Max(num, bucketMax[idx]);
+            } 
+        }
+        
+        //check each bucket, find max gap
+        int preMax = min;
+        int maxGap = Int32.MinValue;
+        for(int i = 0; i < nums.Length - 1; i++) {
+            if(bucketMin[i] != Int32.MaxValue && bucketMax[i] != Int32.MinValue) {
+                //Console.WriteLine(maxGap + " " +  bucketMin[i] + " " + preMax);
+                maxGap = Math.Max(maxGap, bucketMin[i] - preMax);
+                preMax = bucketMax[i];
+            }
+        }
+        maxGap = Math.Max(maxGap, max - preMax);
+        return maxGap;
     }
 }
