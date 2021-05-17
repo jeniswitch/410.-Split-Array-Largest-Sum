88c00affe18ba65015ca8cec002057b0ce3f10d8
Jen <31697704+jeniswitch@users.noreply.github.com>
2021-05-17 14:58:29 -0700

    Update score-of-parentheses.cs

--- score-of-parentheses/score-of-parentheses.cs
+++ score-of-parentheses/score-of-parentheses.cs
@@ -1,28 +1,17 @@
 public class Solution {
     public int ScoreOfParentheses(string s) {
-        var stackb = new Stack<int>();
-        var stackRange = new Stack<int[]>();
+        int bal = 0;
+        int count = 0;
         for(int i = 0; i < s.Length; i++) {
-            if(stackb.Count > 0 && s[stackb.Peek()] == '(' && s[i] == ')') {
-                int start = stackb.Pop();
-                if(start + 1 == i) {
-                    stackRange.Push(new int[]{start, i, 1});
-                }
-                else {
-                    int countInside = 0;
-                     while(stackRange.Count > 0 && stackRange.Peek()[0] >= start && stackRange.Peek()[1] <= i) {
-                        countInside += stackRange.Pop()[2];
-                     } 
-                     stackRange.Push(new int[]{start, i, 2 * countInside});
-                }
+            if(s[i] == '(') {
+                bal++;
             }
             else {
-                stackb.Push(i);
+                bal--;
+                if(s[i - 1] == '(') {
+                    count += 1 << bal;
+                }
             }
-        }
-        int count = 0;
-        while(stackRange.Count > 0) {
-            count += stackRange.Pop()[2];
         }
         return count;
     }
