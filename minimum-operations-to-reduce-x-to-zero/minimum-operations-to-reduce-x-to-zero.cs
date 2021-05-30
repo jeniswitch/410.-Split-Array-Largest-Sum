8a3404778e767a368b3db7453b792b69428cebb0
Jen <31697704+jeniswitch@users.noreply.github.com>
2021-05-30 09:33:19 -0700

    Update minimum-operations-to-reduce-x-to-zero.cs

--- minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.cs
+++ minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.cs
@@ -1,25 +1,22 @@
 public class Solution {
     public int MinOperations(int[] nums, int x) {
-        int[] prefixSum = new int[nums.Length + 1];
+        int total = 0;
         for(int i = 0; i < nums.Length; i++) {
-            prefixSum[i + 1] = prefixSum[i] + nums[i];
+            total += nums[i];
         }
-        if(prefixSum[nums.Length] == x) {
-            return nums.Length;
-        }
-        int target = prefixSum[nums.Length] - x;
-        int max = 0;
+        int target = total - x;
+        int max = -1;
         int start = 0;
+        int crrSum = 0;
         for(int i = 0; i < nums.Length; i++) {
-            while(start < nums.Length && prefixSum[i + 1] - prefixSum[start] > target) {
-                start++;
+            crrSum += nums[i];
+            while(start <= i && crrSum > target) {
+                crrSum -= nums[start++];
             }
-            if(prefixSum[i + 1] - prefixSum[start] == target) {
+            if(crrSum == target) {
                 max = Math.Max(i - start + 1, max);
             }
-            //Console.WriteLine("index " + (i + 1) + " " + start);
-            //Console.WriteLine(prefixSum[i + 1] + " " + prefixSum[start]);
         }
-        return max == 0 ? -1 : nums.Length - max;
+        return max != -1? nums.Length - max : max;
     }
 }
