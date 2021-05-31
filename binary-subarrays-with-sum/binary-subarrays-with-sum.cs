bd4b8684a76659afef24711c3e21b7de47d7493d
Jen <31697704+jeniswitch@users.noreply.github.com>
2021-05-31 10:54:37 -0700

    Time: 284 ms (35.29%), Space: 36.6 MB (5.88%) - LeetHub

--- binary-subarrays-with-sum/binary-subarrays-with-sum.cs
+++ binary-subarrays-with-sum/binary-subarrays-with-sum.cs
@@ -1,20 +1,21 @@
 public class Solution {
     public int NumSubarraysWithSum(int[] nums, int goal) {
-        int[] sum = new int[nums.Length + 1];
-        for(int i = 0; i < nums.Length; i++) {
-            sum[i + 1] = sum[i] + nums[i];
-        }
-        var dic = new Dictionary<int, int>();
         int count = 0;
-        foreach(int s in sum) {
-            if(dic.ContainsKey(s)) {
-                count += dic[s];
+        var dic = new Dictionary<int, int>();
+        int sum = 0;
+        foreach(int num in nums) {
+            sum += num;
+            if(dic.ContainsKey(sum - goal)) {
+                count += dic[sum - goal];
             }
-            if(dic.ContainsKey(s + goal)) {
-                dic[s + goal]++;
+            if(sum == goal) {
+                count++;
+            }
+            if(dic.ContainsKey(sum)) {
+                dic[sum]++;
             }
             else {
-                dic[s + goal] = 1;
+                dic[sum] = 1;
             }
         }
         return count;
