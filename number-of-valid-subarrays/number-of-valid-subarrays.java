877ca889cde3eb806c8109df8a8b42169f210493
Jen <31697704+jeniswitch@users.noreply.github.com>
2021-06-20 07:57:54 -0700

    Update number-of-valid-subarrays.java

--- number-of-valid-subarrays/number-of-valid-subarrays.java
+++ number-of-valid-subarrays/number-of-valid-subarrays.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int validSubarrays(int[] nums) {
+    /*public int validSubarrays(int[] nums) {
         var stack = new Stack<Integer>();
         int count = nums.length;
         stack.push(nums[0]);
@@ -9,6 +9,20 @@ class Solution {
             }
             count += stack.size();
             stack.push(nums[i]);
+        }
+        return count;
+    }*/
+    //use array
+    public int validSubarrays(int[] nums) {
+        int[] stack = new int[nums.length];
+        int count = nums.length;
+        stack[0] = nums[0];
+        for(int i = 1, j = 1; i < nums.length; i++) {
+            while(j > 0 && stack[j - 1] > nums[i]) {
+                j--;
+            }
+            count += j;
+            stack[j++] = nums[i];
         }
         return count;
     }
