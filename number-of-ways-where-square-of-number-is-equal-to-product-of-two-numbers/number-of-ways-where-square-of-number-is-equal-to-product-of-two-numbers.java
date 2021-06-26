ce546233ba16ba5b3bc2a92fe89c7e619151f919
Jen <31697704+jeniswitch@users.noreply.github.com>
2021-06-26 07:44:47 -0700

    Update number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers.java

--- number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers/number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers.java
+++ number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers/number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers.java
@@ -1,23 +1,19 @@
 class Solution {
     public int numTriplets(int[] nums1, int[] nums2) {
+        return helper(nums1, nums2) + helper(nums2, nums1);
+    }
+    private int helper(int[] nums1, int[] nums2) {
         int res = 0;
-        for(long num : nums1) {
-            res += helper(num * num, nums2);
-        }
-        for(long num : nums2) {
-            res += helper(num * num, nums1);
+        for(long n1 : nums1) {
+            HashMap<Long, Integer> hm = new HashMap<>();
+            long target = n1 * n1;
+            for(long n2 : nums2) {
+                if(target % n2 == 0) {
+                    res += hm.getOrDefault(target / n2, 0);
+                }
+                hm.put(n2, hm.getOrDefault(n2, 0) + 1);
+            }
         }
         return res;
-    }
-    private int helper(long target, int[] nums) {
-        HashMap<Long, Integer> hm = new HashMap<>();
-        int count = 0;
-        for(long num : nums) {
-            if(target % num == 0) {
-                count += hm.getOrDefault(target / num, 0);
-            }
-            hm.put(num, hm.getOrDefault(num, 0) + 1);
-        }
-        return count;
     }
 }
