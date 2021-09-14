b064fa83da4ab2d34668c376109df184e6338fac
Jen <31697704+jeniswitch@users.noreply.github.com>
2021-09-14 06:46:30 -0700

    Update people-whose-list-of-favorite-companies-is-not-a-subset-of-another-list.java

--- people-whose-list-of-favorite-companies-is-not-a-subset-of-another-list/people-whose-list-of-favorite-companies-is-not-a-subset-of-another-list.java
+++ people-whose-list-of-favorite-companies-is-not-a-subset-of-another-list/people-whose-list-of-favorite-companies-is-not-a-subset-of-another-list.java
@@ -1,50 +1,26 @@
 class Solution {
-    private int[] p;
-    private List<Set<String>> sets;
     public List<Integer> peopleIndexes(List<List<String>> favoriteCompanies) {
+        List<Set<String>> sets = new ArrayList<>();
         int n = favoriteCompanies.size();
-        sets = new ArrayList<>();
         for(List<String> lst : favoriteCompanies) {
             sets.add(new HashSet<>(lst));
         }
-        p = new int[n];
-        for(int i = 0; i < n; i++) {
-            p[i] = i;
-        }
-        for(int i = 0; i < n; i++) {
-            for(int j = i + 1; j < n; j++) {
-                union(i, j);
-            }
-        }
-        HashSet<Integer> s = new HashSet<>();
-        for(int i = 0; i < n; i++) {
-            s.add(find(i));
-        }
         List<Integer> res = new ArrayList<>();
-        res.addAll(s);
-        Collections.sort(res);
-        return res;
-    }
-    private int find(int x) {
-        if(x != p[x]) p[x] = find(p[x]);
-        return p[x];
-    }
-    private void union(int x, int y) {
-        int px = find(x);
-        int py = find(y);
-        if(px != py) {
-            if(check(px, py)) {
-                p[py] = px;
+        for(int i = 0; i < n; i++) {
+            boolean found = false;
+            Set<String> seti = sets.get(i);
+            for(int j = 0; j < n; j++) {
+                Set<String> setj = sets.get(j);
+                if(i != j && seti.size() < setj.size() && setj.containsAll(seti)) {
+                    found = true;
+                    break;
+                }
             }
-            else if(check(py, px)) {
-                p[px] = py;
+            if(!found) {
+                res.add(i);
             }
         }
-    }
-    private boolean check(int x, int y) {
-        Set<String> xs = sets.get(x);
-        Set<String> ys = sets.get(y);
-        if(xs.size() < ys.size()) return false;
-        return xs.containsAll(ys);
+        return res;
+
     }
 }
