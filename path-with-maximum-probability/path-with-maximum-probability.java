d341b7d369e20dae8db1b14240a5dd0d8129c60c
Jen <31697704+jeniswitch@users.noreply.github.com>
2021-10-13 07:19:49 -0700

    Time: 330 ms (7.58%), Space: 117.4 MB (5.60%) - LeetHub

--- path-with-maximum-probability/path-with-maximum-probability.java
+++ path-with-maximum-probability/path-with-maximum-probability.java
@@ -1,33 +1,29 @@
 class Solution {
     public double maxProbability(int n, int[][] edges, double[] succProb, int start, int end) {
         double[] p = new double[n];
-        Map<Integer, List<int[]>> map = new HashMap<>();
+        HashMap<Integer, List<int[]>> map = new HashMap<>();
         for(int i = 0; i < edges.length; i++) {
-            int a = edges[i][0];
-            int b = edges[i][1];
-            map.putIfAbsent(a, new ArrayList<int[]>());
-            map.putIfAbsent(b, new ArrayList<int[]>());
+            int a = edges[i][0]; int b = edges[i][1];
+            map.putIfAbsent(a, new ArrayList<>());
+            map.putIfAbsent(b, new ArrayList<>());
             map.get(a).add(new int[]{b, i});
             map.get(b).add(new int[]{a, i});
         }
-        p[start] = 1d;
-        PriorityQueue<Integer> pq = new PriorityQueue<Integer>(Comparator.comparingDouble(i -> -p[i]));
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        p[start] = 1;
         pq.offer(start);
         while(!pq.isEmpty()) {
             int crr = pq.poll();
-            if(crr == end) {
-                return p[end];
-            }
-            for(int[] lst : map.getOrDefault(crr, Collections.emptyList())) {
-                int nb = lst[0];
-                int idx = lst[1];
-                double newP = p[crr] * succProb[idx];
-                if(newP > p[nb]) {
-                    p[nb] = newP;
+            for(int[] rec : map.getOrDefault(crr, Collections.emptyList())) {
+                int nb = rec[0];
+                int idx = rec[1];
+                if(p[nb] < p[crr] * succProb[idx]) {
+                    p[nb] = p[crr] * succProb[idx];
                     pq.offer(nb);
                 }
             }
         }
-        return 0d;
+        return p[end];
     }
 }
