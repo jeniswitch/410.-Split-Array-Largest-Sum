1eb6367990837ea13c7d8ffaea53cfa88943449a
Jen <31697704+jeniswitch@users.noreply.github.com>
2021-10-17 09:15:39 -0700

    Time: 1 ms (99.99%), Space: 36.3 MB (49.84%) - LeetHub

--- power-of-two/power-of-two.java
+++ power-of-two/power-of-two.java
@@ -1,9 +1,7 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        if(n == 0) {
-            return false;
-        }
-        long x = (long)n;
-        return (x & (-x)) == x;
+        if(n == 0) return false;
+        long l = (long)n;
+        return (l & (-l)) == l;
     }
 }
