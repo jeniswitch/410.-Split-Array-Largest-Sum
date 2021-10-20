312be91ca571cc5f282153a65273491029a4cd0b
Jen <31697704+jeniswitch@users.noreply.github.com>
2021-10-20 06:13:36 -0700

    Time: 318 ms (35.65%), Space: 114.3 MB (58.65%) - LeetHub

--- arithmetic-slices-ii-subsequence/arithmetic-slices-ii-subsequence.java
+++ arithmetic-slices-ii-subsequence/arithmetic-slices-ii-subsequence.java
@@ -1,22 +1,19 @@
 class Solution {
     public int numberOfArithmeticSlices(int[] nums) {
-        int n = nums.length;
+        HashMap<Integer, Integer>[] map = new HashMap[nums.length];
         long res = 0;
-        Map<Integer, Integer>[] count = new Map[n];
-        for(int i = 0; i < n; i++) {
-            count[i] = new HashMap<>(i);
+        for(int i = 0; i < nums.length; i++) {
+            map[i] = new HashMap<>();
             for(int j = 0; j < i; j++) {
                 long d = (long)nums[i] - (long)nums[j];
-                if(d < Integer.MIN_VALUE || d > Integer.MAX_VALUE) {
-                    continue;
-                }
-                int dd = (int)d;
-                int sum = count[j].getOrDefault(dd, 0);
-                int origin = count[i].getOrDefault(dd, 0);
-                count[i].put(dd, origin + sum + 1);
+                if(d < Integer.MIN_VALUE || d > Integer.MAX_VALUE) continue;
+                int diff = (int)d;
+                int sum = map[j].getOrDefault(diff, 0);
+                int crr = map[i].getOrDefault(diff, 0);
+                map[i].put(diff, sum + crr + 1);
                 res += sum;
             }
         }
-        return (int) res;
+        return (int)res;
     }
 }
