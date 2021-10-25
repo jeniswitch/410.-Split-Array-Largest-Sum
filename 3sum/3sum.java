a6d218aa3273f435126cc102b3368e161db0868d
Jen <31697704+jeniswitch@users.noreply.github.com>
2021-10-25 05:42:39 -0700

    Time: 24 ms (69.99%), Space: 56.2 MB (42.68%) - LeetHub

--- 3sum/3sum.java
+++ 3sum/3sum.java
@@ -1,25 +1,26 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        List<List<Integer>> res = new ArrayList<List<Integer>>();
-        for(int i = 0; i < nums.length - 2; i++) {
-            if(i == 0 || nums[i] != nums[i - 1]) {
-                int l = i + 1, r = nums.length - 1, sum = -nums[i];
-                while(l < r) {
-                    if(nums[l] + nums[r] == sum) {
-                        var lst = new ArrayList<Integer>();
-                        res.add(Arrays.asList(nums[i], nums[l], nums[r]));
-                        while(l < r && nums[l] == nums[l + 1]) l++;
-                        while(l < r && nums[r] == nums[r - 1]) r--;
-                        l++; r--;
-                    }
-                    else if(nums[l] + nums[r] < sum) {
-                        l++;
-                    }
-                    else r--;
-                }
+        List<List<Integer>> res = new ArrayList<>();
+        for(int i = 0; i < nums.length && nums[i] <= 0; i++) {
+            if(i == 0 || nums[i - 1] != nums[i]) {
+                helper(nums, i, res);
             }
         }
         return res;
+    }
+    private void helper(int[] nums, int i, List<List<Integer>> res) {
+        int lo = i + 1, hi = nums.length - 1;
+        while(lo < hi) {
+            int sum = nums[i] + nums[lo] + nums[hi];
+            if(sum < 0) lo++;
+            else if(sum > 0) hi--;
+            else {
+                res.add(Arrays.asList(nums[i], nums[lo++], nums[hi--]));
+                while(lo < hi && nums[lo] == nums[lo - 1]) {
+                    lo++;
+                }
+            }
+        }
     }
 }
