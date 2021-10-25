f1417bb35c8a403949ed753de86c157da5bc5d31
Jen <31697704+jeniswitch@users.noreply.github.com>
2021-10-25 06:18:57 -0700

    Time: 2 ms (12.68%), Space: 39.3 MB (24.66%) - LeetHub

--- powx-n/powx-n.java
+++ powx-n/powx-n.java
@@ -1,7 +1,7 @@
 class Solution {
     public double myPow(double x, int n) {
         int N = n;
-        if(N < 1) {
+        if(N < 0) {
             N = -N;
             x = 1 / x;
         }
@@ -9,12 +9,9 @@ class Solution {
     }
     private double helper(double x, int n) {
         if(n == 0) return 1;
-        double half = helper(x, n / 2);
-        if(n % 2 == 0) {
-            return half * half;
-        }
-        else {
-            return half * half * x;
-        }
+        if(n == 1) return x;
+        double ans = helper(x, n / 2);
+        if(n % 2 == 0) return ans * ans;
+        else return ans * ans * x;
     }
 }
