c91b9ec3c8247ec8f1c8ae1c6f3fa1c4924e5d78
Jen <31697704+jeniswitch@users.noreply.github.com>
2021-12-05 09:40:21 -0800

    Time: 8 ms (88.54%), Space: 39.7 MB (51.56%) - LeetHub

--- longest-well-performing-interval/longest-well-performing-interval.java
+++ longest-well-performing-interval/longest-well-performing-interval.java
@@ -1,16 +1,17 @@
 class Solution {
     public int longestWPI(int[] hours) {
-        var hp = new HashMap<Integer, Integer>();
-        int score = 0, res = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int res = 0;
+        int score = 0;
         for(int i = 0; i < hours.length; i++) {
             score += hours[i] > 8 ? 1 : -1;
             if(score > 0) {
                 res = i + 1;
             }
             else {
-                hp.putIfAbsent(score, i);
-                if(hp.containsKey(score - 1)) {
-                    res = Math.max(res, i - hp.get(score - 1));
+                map.putIfAbsent(score, i);
+                if(map.containsKey(score - 1)) {
+                    res = Math.max(res, i - map.get(score - 1));
                 }
             }
         }
