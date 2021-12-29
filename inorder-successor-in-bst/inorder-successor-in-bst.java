c2c0d03c93fd97c98acaa928ebb77b4cd802155b
Jen <31697704+jeniswitch@users.noreply.github.com>
2021-12-29 07:15:14 -0800

    Update inorder-successor-in-bst.java

--- inorder-successor-in-bst/inorder-successor-in-bst.java
+++ inorder-successor-in-bst/inorder-successor-in-bst.java
@@ -8,22 +8,14 @@
  * }
  */
 class Solution {
-    private TreeNode next;
     public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
-        next = null;
-        helper(root, p);
-        return next;
-    }
-    private void helper(TreeNode node, TreeNode p) {
-        if(node == null){
-            return;
-        }
-        if(node.val <= p.val) {
-            helper(node.right, p);
+        if(root == null) return null;
+        if(root.val <= p.val) {
+            return inorderSuccessor(root.right, p);
         }
         else {
-            next = node;
-            helper(node.left, p);
+            TreeNode next = inorderSuccessor(root.left, p);
+            return next == null ? root : next;
         }
     }
 }
