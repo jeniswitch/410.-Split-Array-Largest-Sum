5e8ce9f21fc152fa1b3e2cf3dac96a9d167b5dc9
Jen <31697704+jeniswitch@users.noreply.github.com>
2022-01-13 06:22:56 -0800

    Time: 1 ms (26.21%), Space: 41.7 MB (36.50%) - LeetHub

--- 1325-delete-leaves-with-a-given-value/1325-delete-leaves-with-a-given-value.java
+++ 1325-delete-leaves-with-a-given-value/1325-delete-leaves-with-a-given-value.java
@@ -15,31 +15,10 @@
  */
 class Solution {
     public TreeNode removeLeafNodes(TreeNode root, int target) {
-        if(root == null) {
-            return null;
-        }
-        helper(root, target);
-        if(root.left == null && root.right == null && root.val == target) {
-            return null;
-        }
+        if(root == null) return null;
+        if(root.left != null) root.left = removeLeafNodes(root.left, target);
+        if(root.right != null) root.right = removeLeafNodes(root.right, target);
+        if(root.left == root.right && root.val == target) return null;
         return root;
-    }
-    private TreeNode helper(TreeNode node, int target) {
-        if(node == null) return null;
-        if(node.left == null && node.right == null) {
-            return node.val == target ? node : null;
-        }
-        TreeNode left = helper(node.left, target);
-        TreeNode right = helper(node.right, target);
-        if(left != null) {
-            node.left = null;
-        }
-        if(right != null) {
-            node.right = null;
-        }
-        if(node.left == null && node.right == null && node.val == target) {
-            return node;
-        }
-        return null;
     }
 }
