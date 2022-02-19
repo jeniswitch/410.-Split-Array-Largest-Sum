56e1e56116f09cb69b1ac5184212044e31fe8ea8
Jen <31697704+jeniswitch@users.noreply.github.com>
2022-02-19 14:41:11 -0800

    Time: 11 ms (16.63%), Space: 43.2 MB (35.95%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.java
+++ 40-combination-sum-ii/40-combination-sum-ii.java
@@ -3,29 +3,20 @@ class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         Arrays.sort(candidates);
         res = new ArrayList<>();
-        for(int i = 0; i < candidates.length; i++) {
-            if(candidates[i] > target) return res;
-            if(i > 0 && candidates[i] == candidates[i - 1]) continue;
-            List<Integer> lst = new ArrayList<>();
-            lst.add(candidates[i]);
-            dfs(candidates, target, i + 1, lst, candidates[i]);
-        }
+        dfs(candidates, target, 0, new ArrayList<>());
         return res;
     }
-    private void dfs(int[] candidates, int target, int idx, List<Integer> lst, int sum) {
-        if(sum > target) return;
-        if(sum == target) {
+    private void dfs(int[] candidates, int target, int idx, List<Integer> lst) {
+        if(target < 0) return;
+        if(target == 0) {
             res.add(new ArrayList<>(lst));
             return;
         }
-        int last = 0;
         for(int i = idx; i < candidates.length; i++) {
-            if(candidates[i] > target || sum + candidates[i] > target) return;  
-            if(last > 0 && candidates[i] == last) continue;
+            if(i > idx && candidates[i] == candidates[i - 1]) continue;
             lst.add(candidates[i]);
-            dfs(candidates, target, i + 1, lst, sum + candidates[i]);
+            dfs(candidates, target - candidates[i], i + 1, lst);
             lst.remove(lst.size() - 1);
-            last = candidates[i];
         }
     }
 }
