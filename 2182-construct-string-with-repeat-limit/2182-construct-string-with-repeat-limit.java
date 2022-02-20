6a5d86169d08e0280729bb41acba549e839e1094
Jen <31697704+jeniswitch@users.noreply.github.com>
2022-02-19 22:05:47 -0800

    Time: 188 ms (50.00%), Space: 78.5 MB (50.00%) - LeetHub

--- 2182-construct-string-with-repeat-limit/2182-construct-string-with-repeat-limit.java
+++ 2182-construct-string-with-repeat-limit/2182-construct-string-with-repeat-limit.java
@@ -12,20 +12,24 @@ class Solution {
         for(char c = 'z'; c >= 'a'; c--) {
             int idx = c - 'a';
             if(count[idx] > 0) {
-                if(sb.length() == 0 || sb.charAt(sb.length() - 1) == c) {
-                    repeat++;
-                }
-                else {
-                    repeat = 1;
-                }
-                if(repeat <= repeatLimit) {
+                if(repeat < repeatLimit) {
                     count[c - 'a']--;
+                    int newRepeat = 1;
+                    if(sb.length() == 0 || sb.charAt(sb.length() - 1) == c) {
+                        newRepeat += repeat;
+                    }
                     sb.append(c);
-                    if(dfs(count, repeatLimit, repeat, sb)) {
+                    if(dfs(count, repeatLimit, newRepeat, sb)) {
                         return true;
                     }
                     sb.setLength(sb.length() - 1);
                     count[c - 'a']++;
+                }
+                else {
+                    repeat = 0;
+                    if (sb.charAt(sb.length() - 1) != c) {
+                        c++;
+                    }
                 }
                 /*else if(c != 'a') {
                     repeat = 0;
