6054e18c608253dcd2a0303238edd100344bca21
Jen <31697704+jeniswitch@users.noreply.github.com>
2022-03-19 13:52:43 -0700

    Time: 4 ms (33.69%), Space: 48.2 MB (20.03%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.java
+++ 2-add-two-numbers/2-add-two-numbers.java
@@ -9,7 +9,8 @@
  * }
  */
 class Solution {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    //recursion
+    /*public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         return helper(l1, l2, 0);
     }
     private ListNode helper(ListNode l1, ListNode l2, int add) {
@@ -28,5 +29,27 @@ class Solution {
         ListNode crr = new ListNode(s % 10);
         crr.next = helper(l1.next, l2 == null ? null : l2.next, s / 10);
         return crr;
+    }*/
+    
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode crr = dummy;
+        int carry = 0;
+        while(l1 != null || l2 != null || carry != 0) {
+            int sum = 0;
+            if(l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if(l2 != null) {
+                sum += l2.val; 
+                l2 = l2.next;
+            }
+            sum += carry;
+            carry = sum / 10;
+            crr.next = new ListNode(sum % 10);
+            crr = crr.next;
+        }
+        return dummy.next;
     }
 }
