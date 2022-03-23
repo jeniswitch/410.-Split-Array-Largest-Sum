2423593a83ee5372426dae7d3a13f1f79e95dc2a
Jen <31697704+jeniswitch@users.noreply.github.com>
2022-03-23 07:02:10 -0700

    Time: 0 ms (100.00%), Space: 42.1 MB (50.70%) - LeetHub

--- 394-decode-string/394-decode-string.java
+++ 394-decode-string/394-decode-string.java
@@ -1,21 +1,16 @@
 class Solution {
+    private int idx = 0;
     public String decodeString(String s) {
-        Queue<Character> q = new LinkedList<>();
-        for(char c : s.toCharArray()) {
-            q.offer(c);
-        }
-        return helper(q);
-    }
-    private String helper(Queue<Character> q) {
         StringBuilder sb = new StringBuilder();
         int num = 0;
-        while(!q.isEmpty()) {
-            char c = q.poll();
+        while(idx < s.length()) {
+            char c = s.charAt(idx++);
+            
             if(Character.isDigit(c)) {
                 num = num * 10 + c - '0';
             }
             else if(c == '[') {
-                String sub = helper(q);
+                String sub = decodeString(s);
                 for(int i = 0; i < num; i++) {
                     sb.append(sub);
                 }
@@ -27,6 +22,7 @@ class Solution {
             else {
                 sb.append(c);
             }
+
         }
         return sb.toString();
     }
