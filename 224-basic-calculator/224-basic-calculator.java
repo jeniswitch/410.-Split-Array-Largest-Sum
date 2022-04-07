c0cb5ddc58410fa7ae24853f038d5fd44a1b2f3a
Jen <31697704+jeniswitch@users.noreply.github.com>
2022-04-07 05:33:30 -0700

    Time: 13 ms (54.66%), Space: 44.5 MB (47.90%) - LeetHub

---
+++ 224-basic-calculator/224-basic-calculator.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int calculate(String s) {
+        Stack<Integer> stack = new Stack<>();
+        int i = 0;
+        int res = 0;
+        int sign = 1;
+        int num = 0;
+        while(i < s.length()) {
+            char c = s.charAt(i++);
+            if(c == '(') {
+                stack.push(res);
+                stack.push(sign);
+                res = 0;
+                sign = 1;
+                num = 0;
+            }
+            if(Character.isDigit(c)) {
+                num = num * 10 + c - '0';
+            }
+            else if(c == '+') {
+                res += sign * num;
+                sign = 1;
+                num = 0;
+            }
+            else if(c == '-') {
+                res += sign * num;
+                sign = -1;
+                num = 0;
+            }
+            else if(c == ')') {
+                res += sign * num;
+                num = 0;
+                res = res * stack.pop() + stack.pop();
+            }
+        }
+        if(num != 0) res += sign * num;
+        return res;
+    }
+}
