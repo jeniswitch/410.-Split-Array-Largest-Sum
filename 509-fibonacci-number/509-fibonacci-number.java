730144acb37d603f909a282be04c2564ceb15d8a
Jen <31697704+jeniswitch@users.noreply.github.com>
2022-04-09 09:17:49 -0700

    Time: 0 ms (100.00%), Space: 41.2 MB (32.41%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,6 +1,12 @@
 class Solution {
     public int fib(int n) {
+        int[] dp = new int[n + 1];
+        return f(n, dp);
+    }
+    private int f(int n, int[] dp) {
         if(n < 2) return n;
-        return fib(n - 1) + fib(n - 2);
+        if(dp[n] > 0) return dp[n];
+        dp[n] = f(n - 1, dp) + f(n - 2, dp);
+        return dp[n];
     }
 }
