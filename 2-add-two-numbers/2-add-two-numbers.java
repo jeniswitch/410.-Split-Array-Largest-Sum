ada55fcaad8aaf18bd142691ed233e8df2c18b55
Jen <31697704+jeniswitch@users.noreply.github.com>
2022-07-13 09:58:59 -0700

    Time: 5 ms (15.67%), Space: 48.5 MB (9.37%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.java
+++ 2-add-two-numbers/2-add-two-numbers.java
@@ -9,47 +9,28 @@
  * }
  */
 class Solution {
-    //recursion
-    /*public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        return helper(l1, l2, 0);
-    }
-    private ListNode helper(ListNode l1, ListNode l2, int add) {
-        if(l1 == null && l2 == null) {
-            if(add == 0) {
-                return null;
-            }
-            else {
-                return new ListNode(add);
-            }
-        }
-        if(l1 == null) {
-            return helper(l2, l1, add);
-        }
-        int s = l1.val + (l2 == null ? 0 : l2.val) + add;
-        ListNode crr = new ListNode(s % 10);
-        crr.next = helper(l1.next, l2 == null ? null : l2.next, s / 10);
-        return crr;
-    }*/
-    
+    private ListNode head, crr;
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode dummy = new ListNode(0);
-        ListNode crr = dummy;
-        int carry = 0;
-        while(l1 != null || l2 != null || carry != 0) {
-            int sum = 0;
-            if(l1 != null) {
-                sum += l1.val;
-                l1 = l1.next;
-            }
-            if(l2 != null) {
-                sum += l2.val; 
-                l2 = l2.next;
-            }
-            sum += carry;
-            carry = sum / 10;
-            crr.next = new ListNode(sum % 10);
-            crr = crr.next;
+        head = new ListNode(0);
+        crr = head;
+        helper(l1, l2, 0);
+        return head.next;
+    }
+    private void helper(ListNode l1, ListNode l2, int carry) {
+        if(l1 == null && l2 == null && carry == 0) {
+            return;
         }
-        return dummy.next;
+        ListNode node = new ListNode();
+        int sum = carry;
+        if(l1 != null) {
+            sum += l1.val;
+        }
+        if(l2 != null) {
+            sum += l2.val;
+        }
+        node.val = sum % 10;
+        crr.next = node;
+        crr = node;
+        helper(l1 == null ? null : l1.next, l2 == null ? null : l2.next, sum / 10);
     }
 }
