b852bb830f724e3ee795bff59e6f661e03e3a0bd
Jen <31697704+jeniswitch@users.noreply.github.com>
2022-08-05 18:19:40 -0700

    Time: 29 ms (76.32%), Space: 66 MB (70.86%) - LeetHub

--- 267-palindrome-permutation-ii/267-palindrome-permutation-ii.java
+++ 267-palindrome-permutation-ii/267-palindrome-permutation-ii.java
@@ -1,63 +1,36 @@
 class Solution {
     public List<String> generatePalindromes(String s) {
         int[] count = new int[26];
-        String uniq = "";
+        List<String> res = new ArrayList<>();
         for(char c : s.toCharArray()) {
             count[c - 'a']++;
         }
-        System.out.println(uniq);
-        List<String> res = new ArrayList<>();
+        boolean foundOdd = false;
         String center = "";
-        Set<Character> set = new HashSet<>();
-        int odd = 0, even = 0;
-        for(int i = 0; i < 26; i++) {
-            int ct = count[i];
-            if(ct == 0) continue;
-            char c = (char)('a' + i);            
-            
-            if(ct % 2 > 0) {
-                odd++;
-                if(odd > 1) {
-                    return res;
+        for(int i = 0; i < count.length; i++) {
+            if(count[i] > 0) {
+                if(count[i] % 2 > 0) {
+                    if(foundOdd) return res;
+                    foundOdd = true;
+                    center = String.valueOf((char)('a' + i));
+                    count[i]--;
                 }
-                center = String.valueOf(c);
-                set.add(c);
             }
-            else {
-                if(!set.contains(c)) {
-                    uniq += String.valueOf(c);
-                    set.add(c);
-                }
-               even++; 
-            }
-            
         }
-        //if(odd > 1) return res;
-        if(odd == 1 && count[center.charAt(0) - 'a'] > 1) {
-            uniq += center;
-            count[center.charAt(0) - 'a']--;
-        }
-        System.out.println(uniq);
-        helper(uniq, count, center, center, res, s.length());
+        helper(res, center, s.length(), count);
         return res;
     }
-    private void helper(String uniq, int[] count, String center, String str, List<String> res, int len) {
+    private void helper(List<String> res, String str, int len, int[] count) {
         if(str.length() == len) {
             res.add(str);
             return;
         }
-        for(char c : uniq.toCharArray()) {
-            if(count[c - 'a'] > 0) {
-                str = c + str + c;
-                count[c - 'a'] -= 2;
-                helper(uniq, count, center, str, res, len);
-                if(str.length() > 2) {
-                    str = str.substring(1, 2) + str.substring(2, str.length() - 1);
-                }
-                else if(str.length() == 2) {
-                    str = center;
-                }
-                count[c - 'a'] += 2;
+        for(int i = 0; i < count.length; i++) {
+            if(count[i] > 0 && count[i] % 2 == 0) {
+                count[i] -= 2;
+                char c = (char)('a' + i);
+                helper(res, c + str + c, len, count);
+                count[i] += 2;
             }
         }
     }
