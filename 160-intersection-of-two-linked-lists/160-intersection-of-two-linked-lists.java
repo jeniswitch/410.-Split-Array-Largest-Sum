954654467d46a2fc0cf6b170be8715d29963ad5f
Jen <31697704+jeniswitch@users.noreply.github.com>
2022-08-18 11:41:09 -0700

    Time: 2 ms (59.39%), Space: 55.7 MB (24.46%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
@@ -11,50 +11,31 @@
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        LengthTail ltA = getLengthTail(headA);
-        LengthTail ltB = getLengthTail(headB);
-        if(ltA.tail != ltB.tail) {
-            return null;
+        ListNode pA = headA;
+        ListNode pB = headB;
+        int lenA = getLen(headA);
+        int lenB = getLen(headB);
+        while(lenA > lenB) {
+            pA = pA.next;
+            lenA--;
         }
-        ListNode shorter = ltA.len <= ltB.len ? headA : headB;
-        ListNode longer = ltA.len > ltB.len ? headA : headB;
-        
-        ListNode crrShorter = shorter;
-        ListNode crrLonger = movePointer(longer, Math.abs(ltA.len - ltB.len));
-        while(crrShorter != crrLonger) {
-            crrShorter = crrShorter.next;
-            crrLonger = crrLonger.next;
+        while(lenB > lenA) {
+            pB = pB.next;
+            lenB--;
         }
-        return crrShorter;
+        while(pA != pB) {
+            pA = pA.next;
+            pB = pB.next;
+        }
+        return pA;
     }
-    class LengthTail{
-        int len;
-        ListNode tail;
-        public LengthTail(){};
-        public LengthTail(int l, ListNode n){
-            len = l;
-            tail = n;
-        }
-    }
-    private ListNode movePointer(ListNode head, int diff) {
-        if(diff == 0) return head;
+    private int getLen(ListNode head) {
+        int len = 0;
         ListNode crr = head;
-        while(crr != null && diff > 0) {
-            crr = crr.next;
-            diff--;
-        }
-        return crr;
-    }
-    private LengthTail getLengthTail(ListNode head) {
-        ListNode crr = head;
-        LengthTail lt = new LengthTail();
         while(crr != null) {
-            if(crr.next == null) {
-                lt.tail = crr;
-            }
             crr = crr.next;
-            lt.len++;
+            len++;
         }
-        return lt;
+        return len;
     }
 }
