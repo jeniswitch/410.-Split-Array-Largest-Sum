aed147ffefea8b530bb8188702ca69017329836c
Jen <31697704+jeniswitch@users.noreply.github.com>
2022-09-27 05:33:35 -0700

    Time: 1 ms (39.40%), Space: 41.4 MB (76.65%) - LeetHub

--- 225-implement-stack-using-queues/225-implement-stack-using-queues.java
+++ 225-implement-stack-using-queues/225-implement-stack-using-queues.java
@@ -7,13 +7,13 @@ class MyStack {
     }
     
     public void push(int x) {
+        q2.add(x);
         while(!q1.isEmpty()) {
                 q2.add(q1.poll());
         }
-        q1.add(x);
-        while(!q2.isEmpty()){
-            q1.add(q2.poll());
-        }
+        Queue<Integer> temp = q1;
+        q1 = q2;
+        q2 = temp;
     }
     
     public int pop() {
