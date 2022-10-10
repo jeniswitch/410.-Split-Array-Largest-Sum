4f4333b7c2b698d27f95cb889825b698fd6954ae
Jen <31697704+jeniswitch@users.noreply.github.com>
2022-10-10 05:04:35 -0700

    Time: 84 ms (14.22%), Space: 49 MB (75.75%) - LeetHub

---
+++ 0510-inorder-successor-in-bst-ii/0510-inorder-successor-in-bst-ii.java
@@ -0,0 +1,35 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node parent;
+};
+*/
+
+class Solution {
+    public Node inorderSuccessor(Node node) {
+        if(node == null) {
+            return null;
+        }
+        if(node.right != null) {
+            return leftmost(node.right);
+        }
+        else {
+            Node c = node;
+            Node p = node.parent;
+            while(p != null && p.left != c) {
+                c = p;
+                p = p.parent;
+            }
+            return p;
+        }
+    }
+    private Node leftmost(Node node) {
+        while(node != null && node.left != null) {
+            node = node.left;
+        }
+        return node;
+    }
+}
