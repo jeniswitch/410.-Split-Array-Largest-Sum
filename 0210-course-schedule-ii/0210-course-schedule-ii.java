18bb70c96a76056c94ab70a1e63d584432f5e734
Jen <31697704+jeniswitch@users.noreply.github.com>
2022-10-12 06:11:33 -0700

    Time: 13 ms (51.38%), Space: 49.7 MB (40.05%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        Queue<Integer> q = new LinkedList<Integer>();
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        int[] inDegree = new int[numCourses];
+        for(int[] c : prerequisites) {
+            inDegree[c[0]]++;
+            List<Integer> arr = map.getOrDefault(c[1], new ArrayList<>());
+            arr.add(c[0]);
+            map.put(c[1], arr);
+        }
+        for(int i = 0; i < numCourses; i++) {
+            if(inDegree[i] == 0) {
+                q.offer(i);
+            }
+        }
+        if(q.size() == 0) {
+            return new int[0];
+        }
+        int[] res = new int[numCourses];
+        int idx = 0;
+        while(!q.isEmpty()) {
+            int size = q.size();
+            while(size-- > 0) {
+                int course = q.poll();
+                res[idx++] = course;
+                List<Integer> nbs = map.getOrDefault(course, new ArrayList<>());
+                for(Integer nb : nbs) {
+                    inDegree[nb]--;
+                    if(inDegree[nb] == 0) {
+                        q.offer(nb);
+                    }
+                }
+            }
+        }
+        if(idx != numCourses) {
+            return new int[0];
+        }
+        return res;
+    }
+}
